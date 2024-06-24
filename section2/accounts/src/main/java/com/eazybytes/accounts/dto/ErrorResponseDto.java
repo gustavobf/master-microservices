13ee7086e2a2e1c55b7788226c5daaad14189290
Gustavo Batista <gustavobatistafig@hotmail.com>
2024-06-24 20:35:40 -0300

    Enhancing documentation of REST APIs

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.http.*;
 
@@ -7,14 +8,30 @@ import java.time.*;
 
 @Data
 @AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold error response information"
+)
 public class ErrorResponseDto {
 
+    @Schema(
+            description = "API path invoked by client"
+    )
     private String apiPath;
 
+    @Schema(
+            description = "Error code representing the error happened"
+    )
     private HttpStatus errorCode;
 
+    @Schema(
+            description = "Error message representing the error happened"
+    )
     private String errorMessage;
 
+    @Schema(
+            description = "Time representing when the error happened"
+    )
     private LocalDateTime errorTime;
 
 }
